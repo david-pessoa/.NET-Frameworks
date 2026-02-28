b7899fabbf29421cd8b4b2d2682d3bef113c0e45
david-pessoa <pessoa.dvlbp@gmail.com>
2026-02-28 18:48:28 -0300

    retoma com alura

---
+++ NewFolder/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+
+struct Pessoa
+{
+  public string nome;
+  public int idade;
+  public double altura;
+}
+static void Main(string[] args)
+{
+  Pessoa p2 = new Pessoa{
+    nome = "Arthur", idade = 9, altura = 1.35
+  };
+
+  Console.WriteLine(p2.nome);
+}
+}
